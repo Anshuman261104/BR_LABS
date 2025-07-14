77c2f58434a2c62e820ac435cf543952883f4401
Anshuman <anshumansr26@gmail.com>
2025-07-14 11:00:57 +0530

    14-07-2025

---
+++ IDcard.java
@@ -0,0 +1,11 @@
+public class IDcard
+{
+    public static void main(String[]args)
+    {
+        System.out.println("Name = Anshuman Singh");
+        System.out.println("Roll No = 2210990143");
+        System.out.println("Branch = BE CSE 4th Year");
+        System.out.println("Father Name = Hemant Kumar Singh" );
+        System.out.println("Address = #2970 PH-7 SAS NAGAR Mohali");
+    }
+}
