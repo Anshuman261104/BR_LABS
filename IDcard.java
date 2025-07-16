ce2fda7b8e92f205178ea6dbb235b4f095185c0d
Anshuman <anshumansr26@gmail.com>
2025-07-16 12:06:42 +0530

    16-07-2025

--- IDcard.java
+++ IDcard.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class IdCard
+public class 
 {
     public static void main(String[]args)
     {
---
+++ LargestOfThree.java
@@ -0,0 +1,29 @@
+//public class LargestOfThree {
+//    public static void main(String[] args) {
+//        int a = 10, b = 25, c = 15;
+//
+//        int largest = (a > b) ? ((a > c) ? a : c)
+//                : ((b > c) ? b : c);
+//
+//        System.out.println("The largest number is: " + largest);
+//    }
+//}
+
+// User Input //
+
+import java.util.Scanner;
+public class LargestOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        System.out.print("Enter third number: ");
+        int c = sc.nextInt();
+        sc.close();
+        int largest = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+
+        System.out.println("The largest number is: " + largest);
+    }
+}
