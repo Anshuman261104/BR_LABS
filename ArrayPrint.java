3538a8e248074e73822109203180146291769381
Anshuman <anshumansr26@gmail.com>
2025-07-17 12:27:31 +0530

    17-07-2025

---
+++ ArrayPrint.java
@@ -0,0 +1,16 @@
+// Print values present in the array using for loop
+
+// 1D Array
+public class ArrayPrint {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Values in the array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
+
+
+
+
